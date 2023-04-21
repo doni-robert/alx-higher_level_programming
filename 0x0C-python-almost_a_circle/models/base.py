e7473af843506062af6ebd4ba7cc378099a0a92b
doni-robert <irobertndungu@gmail.com>
2023-04-21 18:16:23 +0300

    more tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,6 +28,8 @@ class Base:
         Args:
             list_dictionaries(list): a list of dictionaries
         """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return ("[]")
         return json.dumps(list_dictionaries)
 
     @classmethod
@@ -39,4 +41,54 @@ class Base:
             cls(class): the class name of the object
             list_objs(list): a list of instances who inherits of Base
         """
-        pass
+        list_dic = []
+        if list_objs is None:
+            json_string = []
+        else:
+            for obj in list_objs:
+                list_dic.append(obj.to_dictionary())
+            json_string = obj.to_json_string(list_dic)
+        file_name = cls.__name__ + ".json"
+        with open(file_name, 'w') as f:
+            f.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string representation
+
+        Args:
+            json_string: a string representing dictionaries
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Returns an instance with all the attributes already set
+
+        Args:
+            cls: The class of the instance
+            dictionary: key value pair arguments of attributes
+        """
+        if dictionary is not None:
+            if cls.__name__ == "Square":
+                dummy = cls(1)
+            else:
+                dummy = cls(1, 1)
+            dummy.update(**dictionary)
+            return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """ Returns a list of instances """
+        filename = cls.__name__ + ".json"
+        if not filename:
+            return []
+        with open(filename) as f:
+            read_file = f.read()
+            list_dic = cls.from_json_string(read_file)
+            list_objs = [cls.create(**dic) for dic in list_dic]
+            return list_objs
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,5 +1,7 @@
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 class TestBase(unittest.TestCase):
     def test_id_int(self):
@@ -9,4 +11,12 @@ class TestBase(unittest.TestCase):
     def test_id_none(self):
         b = Base()
         self.assertEqual(b.id, 1)
+
+class TestJsonBase(unittest.TestCase):
+    def test_json_type(self):
+        a = Square(3)
+        self.assertIs(type(Base.to_json_string([a.to_dictionary()])), str)
+
+    def test_json_none(self):
+        self.assertEqual("[]", Base.to_json_string(None))
 
