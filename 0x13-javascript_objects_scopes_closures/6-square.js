42e9baceb74faa1330cd97b6ce3c70a1cfc1713b
doni-robert <irobertndungu@gmail.com>
2023-06-14 20:51:04 +0300

    correct export

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const newSquare = require('./5-square');
 
-module.exports = class Square extends Rectangle {
+module.exports = class Square extends newSquare {
   constructor (size) {
     super(size, size);
   }
