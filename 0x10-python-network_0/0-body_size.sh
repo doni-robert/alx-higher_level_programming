6542461d6d677928f7dad7b844dea48239ef6898
doni-robert <irobertndungu@gmail.com>
2023-07-04 09:25:12 +0300

    trial

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, sends a request and displays the size of the body of the response
+# Takes a URL, sends a request to it and displays the size of the response body
 curl -s "$1" | wc -c
