e5d98050826ee1afde08edc5088bfb9ea4fc0a31
doni-robert <irobertndungu@gmail.com>
2023-08-05 10:20:48 +0300

    add files

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+const $ = window.$;
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  $.each(data.results, function (i, movie) {
+    $('UL#list_movies').append('<li>' + movie.title + '</li>');
+  });
+});
